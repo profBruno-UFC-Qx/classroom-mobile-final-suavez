1bab569ba65d3ea85070eb804f5f276f16fd7b22
Hexcold <henriquepiresh3@gmail.com>
2026-05-30 17:17:24 -0300

    feat: colors, theme and typography

---
+++ client/ProjectStudy/app/src/main/java/com/example/projectstudy/ui/theme/Font.kt
@@ -0,0 +1,14 @@
+package com.example.projectstudy.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.example.projectstudy.R
+
+val Nunito = FontFamily(
+    Font(R.font.nunito)
+)
+
+val NunitoSans = FontFamily(
+    Font(R.font.nunito_sans)
+)
