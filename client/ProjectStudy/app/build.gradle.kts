f30a4e691e81e6b7f4725ef47cb5dcc1dacd866b
Hexcold <henriquepiresh3@gmail.com>
2026-06-16 17:00:49 -0300

    refactor: reutiliza avatar padrão no perfil

--- client/ProjectStudy/app/build.gradle.kts
+++ client/ProjectStudy/app/build.gradle.kts
@@ -81,6 +81,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.material3)
 
     debugImplementation(libs.androidx.compose.ui.tooling)
 
