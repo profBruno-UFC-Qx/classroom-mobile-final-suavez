ee149f3427afbbd8f06b4e8e0635fad458cd0939
marquinvitor <marcosvitorsouzafreire@gmail.com>
2026-07-01 16:05:46 -0300

    chore(network): update local backend IP address

--- client/ProjectStudy/app/src/main/java/com/example/projectstudy/di/NetworkModule.kt
+++ client/ProjectStudy/app/src/main/java/com/example/projectstudy/di/NetworkModule.kt
@@ -31,7 +31,7 @@ object NetworkModule {
     @Singleton
     @Named("BaseUrl")
     fun provideBaseUrl(): String {
-        return "http://192.168.18.254:8000"
+        return "http://10.10.219.190:8000"
     }
 
     @Provides
